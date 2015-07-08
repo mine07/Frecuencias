ab537c52b5230cf8c09777178ac77a2e1d91302c
mine07 <cezar21@hotmail.es>
2015-07-08 12:15:35 -0500

    nuevo proyecto

---
+++ Frecuencias/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace Frecuencias
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(132, 186);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(255, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Generar n";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(150, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Cantidad SubIntervalos (N)";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(153, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(131, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(150, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Frecuencia Esperada";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(153, 106);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(131, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoScroll = true;
+            this.tableLayoutPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.GrowStyle = System.Windows.Forms.TableLayoutPanelGrowStyle.AddColumns;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(336, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 46.875F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 53.125F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 28F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(483, 154);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(819, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(150, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Xo2";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(153, 146);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(68, 20);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(152, 169);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "x2α,n-1";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(153, 185);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(68, 20);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(227, 149);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "n-1 =";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(264, 149);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 13);
+            this.label7.TabIndex = 9;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(229, 188);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 13);
+            this.label8.TabIndex = 10;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(744, 182);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Reiniciar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(150, 59);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(25, 13);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "n = ";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(170, 56);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(79, 20);
+            this.textBox5.TabIndex = 13;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(852, 213);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
---
+++ Frecuencias/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Frecuencias
+{
+    public partial class Form1 : Form
+    {
+        TableLayoutPanel tp = new TableLayoutPanel();
+        public GeneradorMultiplicativoMixto.Form1 f1 = new GeneradorMultiplicativoMixto.Form1();
+        public Form1()
+        {
+            InitializeComponent();
+            tp = this.tableLayoutPanel1;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            List<double> nNum = new List<double>();            
+            nNum.Clear();
+            nNum = gen();
+            bool val = f1.validos;
+            if (nNum == null)
+            {
+                return;
+            }
+            if (nNum.Count == 0)
+            {
+                MessageBox.Show("Favor de generar los numeros en el generador");
+                return;
+            }
+            if (!val)
+            {
+                MessageBox.Show("prueba de generar numeros validos desde el generador");
+                return;
+            }
+            listBox1.DataSource = nNum;
+            var y = textBox1.Text;
+            double fe = 0;
+            int x;
+            if (int.TryParse(y, out x))
+            {
+                fe = nNum.Count / Convert.ToDouble(textBox1.Text);
+                fe = Math.Round(fe, 5);
+            }
+            else
+            {
+                listBox1.DataSource = null;
+                MessageBox.Show("Favor de ingresar una frecuencia valida");
+                return;
+            }
+            textBox2.Text = fe.ToString();
+            label7.Text = (nNum.Count - 1).ToString();
+            textBox4.Enabled = true;
+            separacion(ref nNum, ref fe);
+            button1.Enabled = false;
+            textBox1.Enabled = false;
+        }
+
+        public void separacion(ref List<double> nNum, ref double fe)
+        {
+            if (Convert.ToInt32(textBox1.Text) < 12)
+            {
+                tableLayoutPanel1.AutoScroll = false;
+            }
+            else
+            {
+                tableLayoutPanel1.AutoScroll = true;
+            }
+            tableLayoutPanel1.Controls.Clear();
+            int n = Convert.ToInt32(textBox1.Text);
+            tableLayoutPanel1.ColumnCount = n;
+            TableLayoutColumnStyleCollection styles = this.tableLayoutPanel1.ColumnStyles;
+            foreach (ColumnStyle style in styles)
+            {
+                style.SizeType = SizeType.Percent;
+                style.Width = 100 / n;
+            }
+            List<rango> nRang = new List<rango>();
+            for (int i = 0; i < n; i++)
+            {
+                rango r = new rango();
+                Label lb = new Label();
+                lb.Text = fe.ToString();
+                Label lab = new Label();
+                Label cont = new Label();
+                int valor = Convert.ToInt32(textBox5.Text);
+                r.min = Math.Round((((i) * fe)/valor), 3);
+                r.max = Math.Round((((i + 1) * fe) /valor) , 3);
+                lab.Text = r.min.ToString() + " - " + r.max.ToString();
+                cont.Text = "0";
+                tableLayoutPanel1.Controls.Add(lb, i, 0);
+                tableLayoutPanel1.Controls.Add(lab, i, 2);
+                tableLayoutPanel1.Controls.Add(cont, i, 1);
+                nRang.Add(r);
+            }
+            foreach (var y in nNum)
+            {
+                for (int i = 0; i < nRang.Count; i++)
+                {
+                    if (nRang[i].min <= y && y < nRang[i].max)
+                    {
+                        Label lab = (Label)tableLayoutPanel1.GetControlFromPosition(i, 1);
+                        lab.Text = (Convert.ToInt32(lab.Text) + 1).ToString();
+                    }
+                }
+            }
+            label3.Text = nNum.Count.ToString();
+            List<sumatoria> nSum = new List<sumatoria>();
+            double storia = 0;
+            for (int i = 0; i < tableLayoutPanel1.ColumnCount; i++)
+            {
+                Label lb = (Label)tableLayoutPanel1.GetControlFromPosition(i, 1);
+                sumatoria s = new sumatoria();
+                s.fo = Convert.ToDouble(lb.Text);
+                nSum.Add(s);
+                s.r = s.fo - fe;
+                s.r = s.r * s.r;
+                storia += s.r;
+            }
+            storia = (1.00 / fe) * storia;
+            storia = Math.Round(storia, 5);
+            textBox3.Text = storia.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Focus();
+        }
+
+        public class rango
+        {
+            public double min { get; set; }
+            public double max { get; set; }
+        }
+
+        public class sumatoria
+        {
+            public double fo { get; set; }
+            public double r { get; set; }
+        }
+
+        public void limpiarcontroles()
+        {
+            listBox1.DataSource = null;
+            tableLayoutPanel1.Controls.Clear();
+            tableLayoutPanel1.ColumnCount = 1;
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox4.Enabled = false;
+            label3.Text = "";
+            label7.Text = "";
+            label8.Text = "";
+            button1.Enabled = true;
+            textBox1.Enabled = true;
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            double d;
+            bool isD = double.TryParse(textBox4.Text, out d);
+            if(!isD)
+            {
+                if (textBox4.Text != "")
+                {
+                    MessageBox.Show("El texto ingresado tiene que ser tipo double.");
+                }
+                textBox4.Text = "";
+                return;                
+            }
+            double xo = Convert.ToDouble(textBox3.Text);
+            double z = Convert.ToDouble(textBox4.Text);
+            if(xo<z)
+            {
+                label8.Text = "Xo es menor que z por lo tanto los numeros son aceptados";
+            }
+            else
+            {
+                label8.Text = "Xo no es menor que z por lo tanto los numeros no son aceptados";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            limpiarcontroles();
+            f1 = new GeneradorMultiplicativoMixto.Form1();
+        }
+
+        public List<double> gen()
+        {
+            int n = Convert.ToInt32(textBox5.Text);
+            List<double> gene = new List<double>();
+            f1.ShowDialog();
+            List<GeneradorMultiplicativoMixto.Form1.item> lItem = f1.gen;
+            if (n < lItem.Count)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    gene.Add(lItem[i].NumeroRectangular);
+                }
+            }
+            else
+            {
+                MessageBox.Show("No se generaron suficientes numeros para el requisito de n");
+                gene = null;
+            }
+            return gene;
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            string txt = ((TextBox)sender).Text;
+            try
+            {
+                Convert.ToInt64(txt);
+                button1.Enabled = true;
+            }
+            catch
+            {
+                button1.Enabled = false;
+                return;
+            }
+        }
+
+    }
+}
